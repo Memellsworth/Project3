5299dc00328de3f58e01ce40322d772cfb5348e6
chernika81 <chernika81@yahoo.com>
2023-07-11 11:53:11 -0500

    mongodb.ipynb
    created a database using mongodb, imported all the tables.

--- drafts/armine/QuickDBD-export-5 12.31.25 AM.sql
+++ drafts/armine/QuickDBD-export-5 12.31.25 AM.sql
@@ -2,7 +2,7 @@
 
 -- DROP DATABASE IF EXISTS main_db;
 
-CREATE DATABASE main_db
+CREATE DATABASE final_db
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
@@ -17,8 +17,7 @@ CREATE DATABASE main_db
 
 
 CREATE TABLE "IAH_Airline_Delay_Cause" (
-    
-    "year" INTEGER NOT NULL, 
+    "year" INTEGER   NOT NULL,
     "month" INTEGER   NOT NULL,
     "carrier" VARCHAR   NOT NULL,
     "carrier_name" VARCHAR   NOT NULL,
@@ -38,9 +37,58 @@ CREATE TABLE "IAH_Airline_Delay_Cause" (
     "weather_delay" FLOAT   NOT NULL,
     "nas_delay" FLOAT   NOT NULL,
     "security_delay" FLOAT   NOT NULL,
-    "late_aircraft_delay" FLOAT   NOT NULL
-  
+    "late_aircraft_delay" FLOAT   NOT NULL,
+    CONSTRAINT "pk_IAH_Airline_Delay_Cause" PRIMARY KEY (
+        "carrier"
+     )
 );
+
+CREATE TABLE "carrier_group" (
+    "Carrier Code" VARCHAR   NOT NULL,
+    "Date (MM/DD/YYYY)" INTEGER   NOT NULL,
+    "Flight Number" INTEGER   NOT NULL,
+    "Tail Number" INTEGER   NOT NULL,
+    "Destination Airport" INTEGER   NOT NULL,
+    "Scheduled departure time" INTEGER   NOT NULL,
+    "Actual departure time" INTEGER   NOT NULL,
+    "Scheduled elapsed time (Minutes)" INTEGER   NOT NULL,
+    "Actual elapsed time (Minutes)" INTEGER   NOT NULL,
+    "Departure delay (Minutes)" INTEGER   NOT NULL,
+    "Wheels-off time" INTEGER   NOT NULL,
+    "Taxi-Out time (Minutes)" INTEGER   NOT NULL,
+    "Delay Carrier (Minutes)" INTEGER   NOT NULL,
+    "Delay Weather (Minutes)" INTEGER   NOT NULL,
+    "Delay National Aviation System (Minutes)" INTEGER   NOT NULL,
+    "Delay Security (Minutes)" INTEGER   NOT NULL,
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER   NOT NULL,
+    CONSTRAINT "pk_carrier_group" PRIMARY KEY (
+        "Carrier Code"
+     )
+);
+
+CREATE TABLE "dest_airport" (
+    "Destination Airport" VARCHAR   NOT NULL,
+    "Carrier Code" INTEGER   NOT NULL,
+    "Date (MM/DD/YYYY)" INTEGER   NOT NULL,
+    "Flight Number" INTEGER   NOT NULL,
+    "Tail Number" INTEGER   NOT NULL,
+    "Scheduled departure time" INTEGER   NOT NULL,
+    "Actual departure time" INTEGER   NOT NULL,
+    "Scheduled elapsed time (Minutes)" INTEGER   NOT NULL,
+    "Actual elapsed time (Minutes)" INTEGER   NOT NULL,
+    "Departure delay (Minutes)" INTEGER   NOT NULL,
+    "Wheels-off time" INTEGER   NOT NULL,
+    "Taxi-Out time (Minutes)" INTEGER   NOT NULL,
+    "Delay Carrier (Minutes)" INTEGER   NOT NULL,
+    "Delay Weather (Minutes)" INTEGER   NOT NULL,
+    "Delay National Aviation System (Minutes)" INTEGER   NOT NULL,
+    "Delay Security (Minutes)" INTEGER   NOT NULL,
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER   NOT NULL,
+    CONSTRAINT "pk_dest_airport" PRIMARY KEY (
+        "Destination Airport"
+     )
+);
+
 
 CREATE TABLE "AS" (
     
@@ -60,7 +108,10 @@ CREATE TABLE "AS" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_AS" PRIMARY KEY (
+        "Carrier Code"
+     )
 );
 
 CREATE TABLE "EV" (
@@ -81,7 +132,10 @@ CREATE TABLE "EV" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_EV" PRIMARY KEY (
+        "Carrier Code"
+     )
    
 );
 
@@ -103,7 +157,10 @@ CREATE TABLE "UA" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_UA" PRIMARY KEY (
+        "Carrier Code"
+     )
   
 );
 
@@ -125,7 +182,10 @@ CREATE TABLE "NK" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_NK" PRIMARY KEY (
+        "Carrier Code"
+     )
     
 );
 
@@ -147,7 +207,10 @@ CREATE TABLE "WN" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_WN" PRIMARY KEY (
+        "Carrier Code"
+     )
     
 );
 
@@ -169,7 +232,10 @@ CREATE TABLE "B6" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_B6" PRIMARY KEY (
+        "Carrier Code"
+     )
     
 );
 
@@ -191,7 +257,10 @@ CREATE TABLE "F9" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_F9" PRIMARY KEY (
+        "Carrier Code"
+     )
     
 );
 
@@ -213,7 +282,10 @@ CREATE TABLE "MQ" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_MQ" PRIMARY KEY (
+        "Carrier Code"
+     )
    
 );
 
@@ -235,7 +307,10 @@ CREATE TABLE "9E" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_9E" PRIMARY KEY (
+        "Carrier Code"
+     )
     
 );
 
@@ -257,7 +332,10 @@ CREATE TABLE "OO" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_OO" PRIMARY KEY (
+        "Carrier Code"
+     )
     
 );
 
@@ -279,7 +357,10 @@ CREATE TABLE "AA" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_AA" PRIMARY KEY (
+        "Carrier Code"
+     )
    
 );
 
@@ -302,14 +383,17 @@ CREATE TABLE "DL" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_DL" PRIMARY KEY (
+        "Carrier Code"
+     )
     
 );
 
 CREATE TABLE "YX" (
     
     
-     "Carrier Code" VARCHAR   NOT NULL,
+    "Carrier Code" VARCHAR   NOT NULL,
     "Date (MM/DD/YYYY)" DATE   NOT NULL,
     "Flight Number" INTEGER   NOT NULL,
     "Tail Number" VARCHAR,
@@ -325,13 +409,56 @@ CREATE TABLE "YX" (
     "Delay Weather (Minutes)" INTEGER,
     "Delay National Aviation System (Minutes)" INTEGER,
     "Delay Security (Minutes)" INTEGER,
-    "Delay Late Aircraft Arrival (Minutes)" INTEGER
+    "Delay Late Aircraft Arrival (Minutes)" INTEGER,
+     CONSTRAINT "pk_YX" PRIMARY KEY (
+        "Carrier Code"
+     )
    
 );
 
 
+ALTER TABLE "IAH_Airline_Delay_Cause" ADD CONSTRAINT "fk_IAH_Airline_Delay_Cause_carrier" FOREIGN KEY("carrier")
+REFERENCES "carrier_group" ("Carrier Code");
 
 
+ALTER TABLE "AS" ADD CONSTRAINT "fk_AS_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "EV" ADD CONSTRAINT "fk_EV_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "UA" ADD CONSTRAINT "fk_UA_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "NK" ADD CONSTRAINT "fk_NK_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "WN" ADD CONSTRAINT "fk_WN_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "B6" ADD CONSTRAINT "fk_B6_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "F9" ADD CONSTRAINT "fk_F9_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "MQ" ADD CONSTRAINT "fk_MQ_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "9E" ADD CONSTRAINT "fk_9E_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "OO" ADD CONSTRAINT "fk_OO_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "AA" ADD CONSTRAINT "fk_AA_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "DL" ADD CONSTRAINT "fk_DL_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
+
+ALTER TABLE "YX" ADD CONSTRAINT "fk_YX_Carrier_Code" FOREIGN KEY("Carrier Code")
+REFERENCES "carrier_group" ("Carrier Code");
 
 
 
