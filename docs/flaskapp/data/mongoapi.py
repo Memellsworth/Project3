f2061a8b49a149cc208b93954190cec5f70dea28
kuromasadev <kuromasadev@gmail.com>
2023-07-13 23:10:49 -0500

    testing tables and maps

--- docs/flaskapp/data/mongoapi.py
+++ docs/flaskapp/data/mongoapi.py
@@ -1,3 +1,4 @@
+## delays
 import requests
 import json
 url = "https://us-east-1.aws.data.mongodb-api.com/app/data-qbahe/endpoint/data/v1/action/findOne"
@@ -20,3 +21,32 @@ headers = {
 response = requests.request("POST", url, headers=headers, data=payload)
 
 print(response.text)
+## ---------
+
+## airports
+import requests
+import json
+url = "https://us-east-1.aws.data.mongodb-api.com/app/data-humkw/endpoint/data/v1/action/findOne"
+
+payload = json.dumps({
+    "collection": "us_airports",
+    "database": "maindb",
+    "dataSource": "FlightDelay0",
+    "projection": {
+        "_id": 1
+    }
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Request-Headers': '*',
+  'api-key': 'Bszu66RuPai06pjS5dehiObQNQCqcH80jiBKqy91lVmijlcwCRNqAP4uBL52XOnR',
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+# Save the response as a JSON file
+json_data = response.json()
+
+with open('response.json', 'w') as file:
+    json.dump(json_data, file)
+
