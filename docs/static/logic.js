5bc41166acbadab9cfbfc87568e14fcf1bbde2c8
kuromasadev <kuromasadev@gmail.com>
2023-07-15 15:31:19 -0500

    update 3

--- docs/static/logic.js
+++ docs/static/logic.js
@@ -11,63 +11,66 @@ document.addEventListener("DOMContentLoaded", function() {
   }).addTo(map);
 
   // Load the airport locations from the JSON file using D3
-  d3.json('../flaskapp/data/airports.json')
+  d3.json('./flaskapp/data/airports.json')
     .then(function(data) {
-      // Set up a scale for color based on the airport's score
-      var colorScale = d3.scaleSequential()
-        .domain(d3.extent(data, function(d) { return d.score; }))
-        .interpolator(d3.interpolateBlues);
-
-      // Create an SVG layer for drawing the hexbins
-      var svgLayer = L.svg();
-      svgLayer.addTo(map);
-
-      // Get the SVG container
-      var svg = d3.select(svgLayer._container);
-
-      // Generate the hexbin layout
-      var hexbin = d3.hexbin()
-        .radius(10) // Adjust the hexbin size as needed
-        .extent([[0, 0], [map.getSize().x, map.getSize().y]]);
-
-      // Compute the hexbin data based on airport locations
-      var hexbinData = hexbin(data.map(function(d) {
-        return map.latLngToLayerPoint([d.latitude_deg, d.longitude_deg]);
-      }));
-
-      // Create the hexagonal bins
-      svg.append("g")
-        .selectAll("path")
-        .data(hexbinData)
-        .enter()
-        .append("path")
-        .attr("d", function(d) { return hexbin.hexagon(); })
-        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-        .attr("fill", function(d) { return colorScale(d3.mean(d, function(v) { return v.score; })); })
-        .attr("stroke", "white")
-        .attr("stroke-width", 0.5)
-        .on("mouseover", function(d) {
-          // Add tooltip or other interactions as needed
-          console.log("Bin count:", d.length);
-        })
-        .on("mouseout", function(d) {
-          // Remove tooltip or other interactions as needed
-        });
-
-      // Update the SVG layer when the map is panned or zoomed
-      map.on("moveend", update);
-
-      function update() {
-        // Recompute the hexbin data based on updated airport locations
-        hexbinData = hexbin(data.map(function(d) {
-          return map.latLngToLayerPoint([d.latitude_deg, d.longitude_deg]);
-        }));
-
-        // Update the hexagonal bins' positions
-        svg.selectAll("path")
-          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-      }
+      L.geoJSON(data).addTo(map)
     });
+
+  // Set up a scale for color based on the airport's score
+  var colorScale = d3.scaleSequential()
+    .domain(d3.extent(data, function(d) { return d.score; }))
+    .interpolator(d3.interpolateBlues);
+
+  // Create an SVG layer for drawing the hexbins
+  var svgLayer = L.svg();
+  svgLayer.addTo(map);
+
+  // Get the SVG container
+  var svg = d3.select(svgLayer._container);
+
+  // Generate the hexbin layout
+  var hexbin = d3.hexbin()
+    .radius(10) // Adjust the hexbin size as needed
+    .extent([[0, 0], [map.getSize().x, map.getSize().y]]);
+
+  // Compute the hexbin data based on airport locations
+  var hexbinData = hexbin(data.map(function(d) {
+    return map.latLngToLayerPoint([d.latitude_deg, d.longitude_deg]);
+  }));
+
+  // Create the hexagonal bins
+  svg.append("g")
+    .selectAll("path")
+    .data(hexbinData)
+    .enter()
+    .append("path")
+    .attr("d", function(d) { return hexbin.hexagon(); })
+    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+    .attr("fill", function(d) { return colorScale(d3.mean(d, function(v) { return v.score; })); })
+    .attr("stroke", "white")
+    .attr("stroke-width", 0.5)
+    .on("mouseover", function(d) {
+      // Add tooltip or other interactions as needed
+      console.log("Bin count:", d.length);
+    })
+    .on("mouseout", function(d) {
+      // Remove tooltip or other interactions as needed
+    });
+
+  // Update the SVG layer when the map is panned or zoomed
+  map.on("moveend", update);
+
+  function update() {
+    // Recompute the hexbin data based on updated airport locations
+    hexbinData = hexbin(data.map(function(d) {
+      return map.latLngToLayerPoint([d.latitude_deg, d.longitude_deg]);
+    }));
+
+    // Update the hexagonal bins' positions
+    svg.selectAll("path")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  }
+
 });
 
 
