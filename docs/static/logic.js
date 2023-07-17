5e26d065036f036c4e54354221ceca6e35906f2d
kuromasadev <kuromasadev@gmail.com>
2023-07-16 21:01:34 -0500

    updating live front page
    adjusted pathing to csv instead of tsv

--- docs/static/logic.js
+++ docs/static/logic.js
@@ -1,12 +1,12 @@
-d3.tsv("./airlines.tsv").then(function(data) {
+d3.csv("./flaskapp/data/carrier_key_table.csv").then(function(data) {
   // Create dropdown options
   let dropdown = d3.select("#airlines-dropdown");
   dropdown.selectAll("option")
     .data(data)
     .enter()
     .append("option")
-    .text(d => d.name)
-    .attr("value", d => d.name);
+    .text(d => d['Airline Carrier'])
+    .attr("value", d => d['Airline Carrier']);
 
   // Show data for the first airline initially
   showData(data[0]);
@@ -14,13 +14,13 @@ d3.tsv("./airlines.tsv").then(function(data) {
   // Update data when a different option is selected
   dropdown.on("change", function() {
     let selectedName = this.value;
-    let selectedData = data.find(d => d.name === selectedName);
+    let selectedData = data.find(d => d['Airline Carrier'] === selectedName);
     showData(selectedData);
   });
 });
 
 // Function to show data for a given airline
-function showData(airline) {
-  d3.select("#airline-logo").attr("src", airline.logo_url);
-  d3.select("#other-data").html(`Headquarter: ${airline.headquarter}<br> Hub 1: ${airline.hub1}<br> Hub 2: ${airline.hub2}`);
+function showData(data) {
+  d3.select("#airline-logo").attr("src", data['Logo']);
+  d3.select("#other-data").html(`Headquarter: ${data['Headquarter City']}<br> Nearest Hub Airport Code: ${data['Nearest Base']}<br> Hub City: ${data['Nearest Base City']}`);
 }
